2a487ed0931724a4be7502e049f05581f656d638
BenFrancka <benfrancka@gmail.com>
2021-09-29 16:47:36 -0700

    adds colorReducer to app with useReducer, app is functioning as intended and tests are still passing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -8,21 +8,32 @@ export default function App() {
 
   return (
     <>
-      <button aria-label="undo-button" onClick={undo}>
+      <button
+        aria-label="undo-button"
+        onClick={() => dispatch({ type: 'undo' })}
+      >
         undo
       </button>
-      <button aria-label="redo-button" onClick={redo}>
+      <button
+        aria-label="redo-button"
+        onClick={() => dispatch({ type: 'redo' })}
+      >
         redo
       </button>
       <input
         aria-label="color-picker"
         type="color"
-        value={current}
-        onChange={({ target }) => record(target.value)}
+        value={state.current}
+        onChange={({ target }) => dispatch({ 
+          type: 'colorSelection', payload: target.value })}
       />
       <div
         aria-label="display"
-        style={{ backgroundColor: current, width: '10rem', height: '10rem' }}
+        style={{
+          backgroundColor: state.current,
+          width: '10rem',
+          height: '10rem',
+        }}
       ></div>
     </>
   );
